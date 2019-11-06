19e8fc7a18d08e7ab787fe7932699e36e6ee2215
mixmaxze <jose.gama@ccc.ufcg.edu.br>
2019-11-06 20:27:28 -0300

    Mudanças em Pesquisa.

--- src/pesquisa/PesquisaProblemaComparator.java
+++ src/pesquisa/PesquisaProblemaComparator.java
@@ -2,13 +2,35 @@ package pesquisa;
 
 import java.util.Comparator;
 
-public class PesquisaProblemaComparator implements Comparator<Pesquisa>{
+public class PesquisaProblemaComparator implements Comparator<Pesquisa> {
 
 	@Override
 	public int compare(Pesquisa pesquisa, Pesquisa outraPesquisa) {
-		
-		return pesquisa.getProblema().getCodigo().compareTo(outraPesquisa.getProblema().getCodigo());
-		
+
+		if (pesquisa.contemProblema() && pesquisa.contemProblema()) {
+
+			return ((pesquisa.verificaProblema() + pesquisa.getCodigo() + pesquisa.getProblema().getCodigo())
+					.compareTo(outraPesquisa.verificaProblema() + outraPesquisa.getCodigo()
+							+ outraPesquisa.getProblema().getCodigo()));
+		}
+
+		if (pesquisa.contemProblema() && !pesquisa.contemProblema()) {
+			return ((pesquisa.verificaProblema() + pesquisa.getCodigo() + pesquisa.getProblema().getCodigo())
+					.compareTo(outraPesquisa.verificaProblema() + outraPesquisa.getCodigo()));
+		}
+
+		if (!pesquisa.contemProblema() && pesquisa.contemProblema()) {
+
+			return (pesquisa.verificaProblema() + pesquisa.getCodigo().compareTo(outraPesquisa.verificaProblema()
+					+ outraPesquisa.getCodigo() + outraPesquisa.getProblema().getCodigo()));
+
+		}
+
+		else {
+			return (pesquisa.verificaProblema() + pesquisa.getCodigo())
+					.compareTo(outraPesquisa.verificaProblema() + outraPesquisa.getCodigo());
+		}
+
 	}
 
 }
